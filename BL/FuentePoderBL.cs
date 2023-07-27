58c1b907da3177059162598d18f23202b45f9644
Estudiante-06 <Estudiante-06@DESKTOP-1UB9MNJ>
2023-07-27 15:54:07 -0600

    fin primer BL

--- BL/FuentePoderBL.cs
+++ BL/FuentePoderBL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DAL;
+using ET;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,30 @@ namespace BL
 {
     public class FuentePoderBL
     {
-
+        FuentePoderDAL dal = new FuentePoderDAL();
+        public bool IngresarFuentePoder(FuentePoder fuentePoder)
+        {
+            string descripcion = string.Empty;
+            DataTable listaFuentes = dal.BuscarFuentePoder();
+            for (int i = 0; i < listaFuentes.Rows.Count; i++)
+            {
+                descripcion = listaFuentes.Rows[i]["DESCRIPCION"].ToString();
+                if (descripcion.Equals(fuentePoder.Descripcion))
+                    return false;
+            }
+            return dal.IngresarFuentePoder(fuentePoder);
+        }
+        public bool ActualizarFuentePoder(FuentePoder fuentePoder)
+        {
+            return dal.ActualizarFuentePoder(fuentePoder);
+        }
+        public bool EliminarFuentePoder(int id)
+        {
+            return dal.EliminarFuentePoder(id);
+        }
+        public DataTable BuscarFuentePoder()
+        {
+            return dal.BuscarFuentePoder();
+        }
     }
 }
